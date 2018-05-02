51ca657e0fe7371dbfc23b0f4de8b8736705af70
Cornelia <cornelia.schulz.nz@gmail.com>
2018-05-02 22:15:49 +1200

    created snake object

--- js/script.js
+++ js/script.js
@@ -3,8 +3,25 @@ var ctx = c.getContext("2d");
 console.log("game on");
 var backgroundImage = new Image();
 backgroundImage.src="images/grass.jpg";
-/*ctx.moveTo(400, 200);
-ctx.lineTo(440, 220);
-ctx.stroke();*/
-ctx.fillStyle = 'yellow';
-ctx.fillRect(400, 200, 40, 8);
+ctx.fillStyle = 'brown';
+ctx.fillRect(392, 200, 8, 8);
+ctx.fillRect(400, 200, 8, 8);
+ctx.fillRect(408, 200, 8, 8);
+ctx.fillRect(408, 208, 8, 8);
+
+var snakeSegment = function(x, y, sideLength) {
+    this.x = x;
+    this.y = y;
+    this.width = sideLength;
+    this.height = sideLength;
+    this.render = function(ctx){
+        ctx.fillRect;
+    }
+}
+
+var snake = [
+    new snakeSegment(392, 200, 8),
+    new snakeSegment(400, 200, 8),
+    new snakeSegment(408, 200, 8),
+    new snakeSegment(408, 208, 8)
+];
